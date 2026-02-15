70ee5ea99271fb3942ca1e25d5d532260c4d9af7
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2026-02-15 16:53:57 +0900

    Refactor VP case2 via pseudo loop helpers

--- src/pseudo_loop.cc
+++ src/pseudo_loop.cc
@@ -231,7 +231,7 @@ void pseudo_loop::compute_VP(cand_pos_t i, cand_pos_t j, sparse_tree &tree) {
 
     // Hosna April 9th, 2007
     // checking the borders as they may be negative
-    if ((tree.tree[i].parent->index) < (tree.tree[j].parent->index) && (tree.tree[j].parent->index) > 0 && b_ij >= 0 && bp_ij >= 0 && Bp_ij < 0) {
+    if (rules.parent_index(i) < rules.parent_index(j) && rules.parent_index(j) > 0 && b_ij >= 0 && bp_ij >= 0 && Bp_ij < 0) {
         energy_t WI_i_plus_b_minus = get_WI(i + 1, b_ij - 1);
         energy_t WI_bp_plus_j_minus = get_WI(bp_ij + 1, j - 1);
         m2 = WI_i_plus_b_minus + WI_bp_plus_j_minus;
