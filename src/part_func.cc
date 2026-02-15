97130fa8a445ad648e28eec96b9fc86cf18aa480
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2026-02-16 01:05:28 +0900

    Move compute_exterior_cases to pk helpers

--- src/part_func.cc
+++ src/part_func.cc
@@ -10,21 +10,3 @@
 #include <math.h>
 #include <stdio.h>
 #include <string>
-
-/**
- * In cases where the band border is not found, if specific cases are met, the value is Inf(i.e n) not -1.
- * When applied to WMBP, if all cases are 0, then we can proceed with WMBP
- * Mateo Jan 2025: Added to Fix WMBP problem
- */
-int W_final_pf::compute_exterior_cases(cand_pos_t l, cand_pos_t j, sparse_tree &tree) {
-    // Case 1 -> l is not covered
-    bool case1 = tree.tree[l].parent->index <= 0;
-    // Case 2 -> l is paired
-    bool case2 = tree.tree[l].pair > 0;
-    // Case 3 -> l is part of a closed subregion
-    // bool case3 = 0;
-    // Case 4 -> l.bp(l) i.e. l.j does not cross anything -- could I compare parents instead?
-    bool case4 = j < tree.Bp(l, j);
-    // By bitshifting each one, we have a more granular idea of what cases fail and is faster than branching
-    return (case1 << 2) | (case2 << 1) | case4;
-}
--- src/part_func_pk.cc
+++ src/part_func_pk.cc
@@ -45,6 +45,24 @@ void W_final_pf::compute_pk_energies(cand_pos_t i, cand_pos_t j, sparse_tree &tr
     compute_BE(i, ip, jp, j, tree);
 }
 
+/**
+ * In cases where the band border is not found, if specific cases are met, the value is Inf(i.e n) not -1.
+ * When applied to WMBP, if all cases are 0, then we can proceed with WMBP
+ * Mateo Jan 2025: Added to Fix WMBP problem
+ */
+int W_final_pf::compute_exterior_cases(cand_pos_t l, cand_pos_t j, sparse_tree &tree) {
+    // Case 1 -> l is not covered
+    bool case1 = tree.tree[l].parent->index <= 0;
+    // Case 2 -> l is paired
+    bool case2 = tree.tree[l].pair > 0;
+    // Case 3 -> l is part of a closed subregion
+    // bool case3 = 0;
+    // Case 4 -> l.bp(l) i.e. l.j does not cross anything -- could I compare parents instead?
+    bool case4 = j < tree.Bp(l, j);
+    // By bitshifting each one, we have a more granular idea of what cases fail and is faster than branching
+    return (case1 << 2) | (case2 << 1) | case4;
+}
+
 void W_final_pf::compute_WI(cand_pos_t i, cand_pos_t j, sparse_tree &tree) {
     scfg::compute_WI_restricted(*this, i, j, tree);
 }
