8bd24f333fb66713b75faee3a27deffb8eaf8855
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2026-02-16 00:51:41 +0900

    Split rescale helpers from part_func

--- src/part_func.cc
+++ src/part_func.cc
@@ -1,10 +1,8 @@
 #include "part_func.hh"
 #include "dot_plot.hh"
-#include "h_externs.hh"
 #include "scfg/constraint_oracle.hh"
 #include "scfg/part_func_adapter.hh"
 #include "scfg/rules_part_func.hh"
-#include "pf_globals.hh"
 #include "scfg/legacy_adapter.hh"
 
 #include <algorithm>
@@ -12,23 +10,6 @@
 #include <math.h>
 #include <stdio.h>
 #include <string>
-
-/*
- * If the global use_mfelike_energies flag is set, truncate doubles to int
- * values and cast back to double. This makes the energy parameters of the
- * partition (folding get_scaled_exp_params()) compatible with the mfe folding
- * parameters (get_scaled_exp_params()), e.g. for explicit partition function
- * computations.
- */
-#define TRUNC_MAYBE(X) ((!pf_smooth) ? (double)((int)(X)) : (X))
-/* Rescale Free energy contribution according to deviation of temperature from measurement conditions */
-#define RESCALE_dG(dG, dH, dT) ((dH) - ((dH) - (dG)) * dT)
-
-/*
- * Rescale Free energy contribution according to deviation of temperature from measurement conditions
- * and convert it to Boltzmann Factor for specific kT
- */
-#define RESCALE_BF(dG, dH, dT, kT) (exp(-TRUNC_MAYBE((double)RESCALE_dG((dG), (dH), (dT))) * 10. / kT))
 
 W_final_pf::W_final_pf(std::string &seq, std::string &MFE_structure, bool pk_free,bool pk_only,bool fatgraph, int dangle, double energy, int num_samples, bool PSplot)
     : exp_params_(scale_pf_parameters()) {
@@ -82,57 +63,6 @@ W_final_pf::W_final_pf(std::string &seq, std::string &MFE_structure, bool pk_fre
 }
 
 W_final_pf::~W_final_pf() {}
-
-void W_final_pf::exp_params_rescale(double mfe) {
-    double e_per_nt, kT;
-    kT = exp_params_->kT;
-
-    e_per_nt = mfe * 1000. / this->n;
-
-    exp_params_->pf_scale = exp(-(exp_params_->model_details.sfact * e_per_nt) / kT);
-
-    if (exp_params_->pf_scale < 1.) exp_params_->pf_scale = 1.;
-
-    exp_params_->pf_scale = 1.;
-
-    this->scale[0] = 1.;
-    this->scale[1] = (pf_t)(1. / exp_params_->pf_scale);
-    this->expMLbase[0] = 1;
-    this->expMLbase[1] = (pf_t)(exp_params_->expMLbase / exp_params_->pf_scale);
-
-    this->expcp_pen[0] = 1;
-    this->expcp_pen[1] = (pf_t)(expcp_penalty / exp_params_->pf_scale);
-    this->expPUP_pen[0] = 1;
-    this->expPUP_pen[1] = (pf_t)(expPUP_penalty / exp_params_->pf_scale);
-
-    for (cand_pos_t i = 2; i <= this->n; i++) {
-        this->scale[i] = this->scale[i / 2] * this->scale[i - (i / 2)];
-        this->expMLbase[i] = (pf_t)pow(exp_params_->expMLbase, (double)i) * this->scale[i];
-        this->expcp_pen[i] = (pf_t)pow(expcp_penalty, (double)i) * this->scale[i];
-        this->expPUP_pen[i] = (pf_t)pow(expPUP_penalty, (double)i) * this->scale[i];
-    }
-}
-
-void W_final_pf::rescale_pk_globals() {
-    double kT = exp_params_->model_details.betaScale * (exp_params_->model_details.temperature + K0) * GASCONST; /* kT in cal/mol  */
-    double TT = (exp_params_->model_details.temperature + K0) / (Tmeasure);
-    int pf_smooth = exp_params_->model_details.pf_smooth;
-
-    expPS_penalty = RESCALE_BF(PS_penalty, PS_penalty * 3, TT, kT);
-    expPSM_penalty = RESCALE_BF(PSM_penalty, PSM_penalty * 3, TT, kT);
-    expPSP_penalty = RESCALE_BF(PSP_penalty, PSP_penalty * 3, TT, kT);
-    expPB_penalty = RESCALE_BF(PB_penalty, PB_penalty * 3, TT, kT);
-    expPUP_penalty = RESCALE_BF(PUP_penalty, PUP_penalty * 3, TT, kT);
-    expPPS_penalty = RESCALE_BF(PPS_penalty, PPS_penalty * 3, TT, kT);
-
-    expa_penalty = RESCALE_BF(a_penalty, ML_closingdH, TT, kT);
-    expb_penalty = RESCALE_BF(b_penalty, ML_interndH, TT, kT);
-    expc_penalty = RESCALE_BF(c_penalty, ML_BASEdH, TT, kT);
-
-    expap_penalty = RESCALE_BF(ap_penalty, ap_penalty * 3, TT, kT);
-    expbp_penalty = RESCALE_BF(bp_penalty, bp_penalty * 3, TT, kT);
-    expcp_penalty = RESCALE_BF(cp_penalty, cp_penalty * 3, TT, kT);
-}
 
 /**
  * In cases where the band border is not found, if specific cases are met, the value is Inf(i.e n) not -1.
