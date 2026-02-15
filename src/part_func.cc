dfdbda403d4d7da6131c9bae48e1e22bbb3a54f5
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2026-02-16 00:24:06 +0900

    Split energy helpers from part_func

--- src/part_func.cc
+++ src/part_func.cc
@@ -217,49 +217,6 @@ pf_t W_final_pf::hfold_centroid(sparse_tree &tree){
     return dist;
 }
 
-pf_t W_final_pf::exp_Extloop(cand_pos_t i, cand_pos_t j) {
-    pair_type tt = pair[S_[i]][S_[j]];
-
-    if (exp_params_->model_details.dangles == 1 || exp_params_->model_details.dangles == 2) {
-        base_type si1 = i > 1 ? S_[i - 1] : -1;
-        base_type sj1 = j < n ? S_[j + 1] : -1;
-        return exp_E_ExtLoop(tt, si1, sj1, exp_params_);
-    } else {
-        return exp_E_ExtLoop(tt, -1, -1, exp_params_);
-    }
-}
-
-pf_t W_final_pf::exp_MLstem(cand_pos_t i, cand_pos_t j) {
-    pair_type tt = pair[S_[i]][S_[j]];
-    if (exp_params_->model_details.dangles == 1 || exp_params_->model_details.dangles == 2) {
-        base_type si1 = i > 1 ? S_[i - 1] : -1;
-        base_type sj1 = j < n ? S_[j + 1] : -1;
-        return exp_E_MLstem(tt, si1, sj1, exp_params_);
-    } else {
-        return exp_E_MLstem(tt, -1, -1, exp_params_);
-    }
-}
-
-pf_t W_final_pf::exp_Mbloop(cand_pos_t i, cand_pos_t j) {
-    pair_type tt = pair[S_[j]][S_[i]];
-    if (exp_params_->model_details.dangles == 1 || exp_params_->model_details.dangles == 2) {
-        base_type si1 = i > 1 ? S_[i + 1] : -1;
-        base_type sj1 = j < n ? S_[j - 1] : -1;
-        return exp_E_MLstem(tt, sj1, si1, exp_params_);
-    } else {
-        return exp_E_MLstem(tt, -1, -1, exp_params_);
-    }
-}
-
-pf_t W_final_pf::HairpinE(cand_pos_t i, cand_pos_t j) {
-
-    const int ptype_closing = pair[S_[i]][S_[j]];
-    if (ptype_closing == 0) return 0;
-    pf_t e_h = static_cast<pf_t>(exp_E_Hairpin(j - i - 1, ptype_closing, S1_[i + 1], S1_[j - 1], &seq.c_str()[i - 1], exp_params_));
-    e_h *= scale[j - i + 1];
-    return e_h;
-}
-
 pf_t W_final_pf::compute_internal_restricted(cand_pos_t i, cand_pos_t j, std::vector<int> &up) {
     pf_t v_iloop = 0;
     cand_pos_t max_k = std::min(j - TURN - 2, i + MAXLOOP + 1);
@@ -355,27 +312,6 @@ void W_final_pf::compute_VP(cand_pos_t i, cand_pos_t j, sparse_tree &tree) {
     scfg::compute_VP_restricted(*this, i, j, tree);
 }
 
-pf_t W_final_pf::compute_int(cand_pos_t i, cand_pos_t j, cand_pos_t k, cand_pos_t l) {
-    const pair_type ptype_closing = pair[S_[i]][S_[j]];
-    return exp_E_IntLoop(k - i - 1, j - l - 1, ptype_closing, rtype[pair[S_[k]][S_[l]]], S1_[i + 1], S1_[j - 1], S1_[k - 1], S1_[l + 1], exp_params_);
-}
-
-pf_t W_final_pf::get_e_stP(cand_pos_t i, cand_pos_t j) {
-    if (i + 1 == j - 1) { // TODO: do I need something like that or stack is taking care of this?
-        return 0;
-    }
-    pf_t e_st = compute_int(i, j, i + 1, j - 1);
-
-    return pow(e_st, e_stP_penalty);
-}
-
-pf_t W_final_pf::get_e_intP(cand_pos_t i, cand_pos_t ip, cand_pos_t jp, cand_pos_t j) {
-    if (ip == i + 1 && jp == j - 1) return 0;
-    pf_t e_int = compute_int(i, j, ip, jp);
-
-    return pow(e_int, e_intP_penalty);
-}
-
 void W_final_pf::compute_WMBW(cand_pos_t i, cand_pos_t j, sparse_tree &tree) {
     scfg::compute_WMBW_restricted(*this, i, j, tree);
 }
@@ -391,4 +327,3 @@ void W_final_pf::compute_WMB(cand_pos_t i, cand_pos_t j, sparse_tree &tree) {
 void W_final_pf::compute_BE(cand_pos_t i, cand_pos_t j, cand_pos_t ip, cand_pos_t jp, sparse_tree &tree) {
     scfg::compute_BE_restricted(*this, i, j, ip, jp, tree);
 }
-
